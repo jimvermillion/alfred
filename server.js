a211d8a9f4f67d5878fc276e81abb77da70fad50
SamHeutmaker <samheutmaker@gmail.com>
2016-03-06 14:46:48 -0800

    Updated CORS

--- server.js
+++ server.js
@@ -12,6 +12,13 @@ mongoose.connect(db.url);
 const majorA = require('major-a');
 const majorRouter = majorA.majorRouter;
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, authentication, authorization, token');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();
+});
+
 
 // Auth Router
 app.use('/auth', majorRouter);
