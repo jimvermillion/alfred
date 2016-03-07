55bf9149103f245f989ce0c6b1b05bd0cf42301e
SamHeutmaker <samheutmaker@gmail.com>
2016-03-06 17:25:45 -0800

    Fixed some shit on the server

--- routes/dashboard_router.js
+++ routes/dashboard_router.js
@@ -1,26 +1,28 @@
 const express = require('express');
 
+// Config Model
 const Config = require(__dirname + '/../models/config');
+
+// Major A
+const majorA = require('major-a');
+const mAuth = majorA.majorAuth;
+
+// Dashboard Router
 var dashboardRouter = module.exports = exports = express.Router();
-
-
 // Get user preferences
-dashboardRouter.get('/preferences', (req, res) => {
-	console.log(req.user._id);
+dashboardRouter.get('/preferences', mAuth(), (req, res) => {
   // Check db for preferences
   Config.findOne({
     owner_id: req.user._id
   }, (err, data) => {
     // Check error
     if (err) {
-      console.log('err')
       return res.status(500).json({
         msg: 'There was an error'
       });
     }
     // No config, create new config
     if (!data) {
-      console.log('no data');
       var newConfig = new Config;
       newConfig.owner_id = req.user._id;
       // Save Config
